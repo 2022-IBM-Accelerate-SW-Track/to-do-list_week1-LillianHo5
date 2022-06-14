7d6efdef3910ea4dc037cc8d7d680ef1e7abd6f5
LillianHo5 <lmho@ucsd.edu>
2022-06-14 15:05:20 -0700

    Update About.js with About Page Content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_pic from "../assets/profile_pic.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div class="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                src={profile_pic}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Lillian Ho</div>
+              <div className="brief_description">
+                <p>
+                  Hello! My name is Lillian Ho, and I am a rising second year at UC San Diego studying computer science. In 
+                  my free time, I enjoy going on hikes and spending time with my loved ones. I also enjoy playing video
+                  games and occasionally knitting.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
